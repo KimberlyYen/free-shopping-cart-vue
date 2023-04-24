c1f69179abe533d94ba3701deb8c70cebde15f98
kimberly yen <kim223305@example.com>
2023-04-24 10:59:56 +0800

    home page NAV OK

--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -4,26 +4,39 @@
 
 <template>
 
-    <nav class="navbar navbar-light bg-light ">
+    <nav class="navbar navbar-light bg-light r ">
     <div class="container-fluid">
-        <a class="navbar-brand">Navbar</a>
-        <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-        
-        <button class="btn btn-success" type="submit">登入</button>
-        <button class="btn btn-success" type="submit">註冊</button>
-        <button class="btn btn-info" type="submit">我的購物車</button>
+      <div class="logo border rounded-circle d-flex align-items-center justify-content-center">
+        <a class="navbar-brand mx-auto">LOGO</a>
+      </div>
 
+        <div>
+          <div>
+            <button class="btn btn-success rounded-5" type="submit">登入</button>
+            <button class="btn btn-success rounded-5" type="submit">註冊</button>
+          </div>
 
-        </form>
+          <form class="d-flex flex-row align-items-center mt-2">
+            <input class="form-control w-100" type="search" placeholder="商品名" aria-label="Search">
+            <button class="btn btn-outline-success w-50 mx-2" type="submit">搜索</button>
+            <button class="btn btn-info w-75" type="submit">我的購物車</button>
+          </form>
+            
+
+  
+        </div>
     </div>
     </nav>
 
 </template>
 
 <style scoped>
-.read-the-docs {
+/* .read-the-docs {
   color: #888;
+} */
+
+.logo {
+  width: 100px;
+  height: 100px;
 }
 </style>
